2e4db7b4bd72d3e76a38e941755338c1aac58b20
FeroxRev <Ferox2552@gmail.com>
2016-07-23 16:14:04 +0200

    Initial commit

---
+++ PokemonGo.RocketAPI/ISettings.cs
@@ -0,0 +1,17 @@
+﻿using PokemonGo.RocketAPI.Enums;
+using PokemonGo.RocketAPI.GeneratedCode;
+using System.Collections.Generic;
+
+namespace PokemonGo.RocketAPI
+{
+    public interface ISettings
+    {
+        AuthType AuthType { get; }
+        double DefaultLatitude { get; }
+        double DefaultLongitude { get; }
+        double DefaultAltitude { get; }
+        string GoogleRefreshToken { get; set; }
+        string PtcPassword { get; }
+        string PtcUsername { get; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Pokemon-Go-Rocket-API
-Client API Library
+# Pokemon Go API Client Library Proof of Concept#
+
+Example:
+
+```
+
+var client = new Client(new Settings()); //Define your own ISettings implementation
+await _client.DoGoogleLogin();
+await _client.SetServer();
+var profile = await _client.GetProfile();
+var settings = await _client.GetSettings();
+var mapObjects = await _client.GetMapObjects();
+```
