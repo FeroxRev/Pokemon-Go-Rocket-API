4d9bb8f5cf1a3477de8663b55a89c7e5fc4cc12b
FeroxRev <Ferox2552@gmail.com>
2016-07-29 13:32:48 +0200

    Upgrade proto files

--- PokemonGo.RocketAPI/Rpc/Encounter.cs
+++ PokemonGo.RocketAPI/Rpc/Encounter.cs
@@ -28,13 +28,13 @@ namespace PokemonGo.RocketAPI.Rpc
             return await PostProtoPayload<Request, EncounterResponse>(RequestType.Encounter, message);
         }
 
-        public async Task<UseItemCaptureResponse> UseCaptureItem(ulong encounterId, ItemId itemId, string spawnPointGuid)
+        public async Task<UseItemCaptureResponse> UseCaptureItem(ulong encounterId, ItemId itemId, string spawnPointId)
         {
             var message = new UseItemCaptureMessage
             {
                 EncounterId = encounterId,
                 ItemId = itemId,
-                SpawnPointGuid = spawnPointGuid
+                SpawnPointId = spawnPointId
             };
             
             return await PostProtoPayload<Request, UseItemCaptureResponse>(RequestType.UseItemCapture, message);
@@ -80,7 +80,7 @@ namespace PokemonGo.RocketAPI.Rpc
             return await PostProtoPayload<Request, DiskEncounterResponse>(RequestType.DiskEncounter, message);
         }
 
-        public async Task<EncounterTutorialCompleteResponse> EncounterTutorialComplete(PokemonId pokemonId)
+        public async Task<EncounterTutorialCompleteResponse> EncounterLurePokemon(PokemonId pokemonId)
         {
             var message = new EncounterTutorialCompleteMessage()
             {
