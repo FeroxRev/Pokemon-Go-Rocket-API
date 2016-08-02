92de53619ee925a0454adb9f6696a1beeedfaee2
Da-Teach <da_teach@thehackerwithin.com>
2016-08-02 11:24:46 +0200

    Added multi-message decoding (#93)
    * Fix HintPath
    
    * Added #pragma for async-warning
    
    * Added multi-message decode and made GetMapObjects return all messages

--- PokemonGo.RocketAPI/Exceptions/InvalidResponseException.cs
+++ PokemonGo.RocketAPI/Exceptions/InvalidResponseException.cs
@@ -4,5 +4,11 @@ namespace PokemonGo.RocketAPI.Exceptions
 {
     public class InvalidResponseException : Exception
     {
+        public InvalidResponseException()
+        {}
+
+        public InvalidResponseException(string message)
+            : base(message)
+        { }
     }
 }
