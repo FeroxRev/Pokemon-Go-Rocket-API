eee68e59e995d347c4bd11d3990e94fa68690992
neoz <neoz@gmail.com>
2016-08-07 17:19:06 +0700

    - Fix first packet with null AuthTicket

--- PokemonGo.RocketAPI/Signatures/GenerateSignature.cs
+++ PokemonGo.RocketAPI/Signatures/GenerateSignature.cs
@@ -76,33 +76,36 @@ namespace PokemonGo.RocketAPI.Signatures
         {
             var sign = new POGOProtos.Signature.Signature();
 
-            sign.LocationHash1 = (uint)generateLocation1(request.AuthTicket, request.Latitude, request.Longitude, request.Altitude);
-            sign.LocationHash2 = (uint)generateLocation2(request.AuthTicket, request.Latitude, request.Longitude, request.Altitude);
-            foreach (Request req in request.Requests)
+            if (request.AuthTicket != null)
             {
-                var hash = generateRequestHash(request.AuthTicket, req);
-                sign.RequestHash.Add(hash);
-            }
-
-            Random rnd = new Random();
-            Byte[] bytes = new Byte[32];
-            rnd.NextBytes(bytes);
-
-            sign.Unk22 = Google.Protobuf.ByteString.CopyFrom(bytes);
-            sign.Timestamp = (ulong)DateTime.UtcNow.ToUnixTime();
-            sign.TimestampSinceStart = sign.Timestamp - START_TIME;
-
-            byte[] gensign = generatesign(sign);
-
-            Unknown6 u6 = new Unknown6()
-            {
-                RequestType = 6,
-                Unknown2 = new Unknown6.Types.Unknown2
+                sign.LocationHash1 = (uint)generateLocation1(request.AuthTicket, request.Latitude, request.Longitude, request.Altitude);
+                sign.LocationHash2 = (uint)generateLocation2(request.AuthTicket, request.Latitude, request.Longitude, request.Altitude);
+                foreach (Request req in request.Requests)
                 {
-                    Unknown1 = Google.Protobuf.ByteString.CopyFrom(gensign)
+                    var hash = generateRequestHash(request.AuthTicket, req);
+                    sign.RequestHash.Add(hash);
                 }
-            };
-            request.Unknown6.Add(u6);
+
+                Random rnd = new Random();
+                Byte[] bytes = new Byte[32];
+                rnd.NextBytes(bytes);
+
+                sign.Unk22 = Google.Protobuf.ByteString.CopyFrom(bytes);
+                sign.Timestamp = (ulong)DateTime.UtcNow.ToUnixTime();
+                sign.TimestampSinceStart = sign.Timestamp - START_TIME;
+
+                byte[] gensign = generatesign(sign);
+
+                Unknown6 u6 = new Unknown6()
+                {
+                    RequestType = 6,
+                    Unknown2 = new Unknown6.Types.Unknown2
+                    {
+                        Unknown1 = Google.Protobuf.ByteString.CopyFrom(gensign)
+                    }
+                };
+                request.Unknown6.Add(u6);
+            }
 
             return request;
         }
